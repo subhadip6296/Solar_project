54c39e541677561a22e1efbb5a0a1a0eb02e6798
Bhavesh Singh <bhavesh.singhjayash@gmail.com>
2024-08-23 00:13:44 +0530

    PartnersAdded

--- client/src/components/homeCompo/Partner.jsx
+++ client/src/components/homeCompo/Partner.jsx
@@ -3,19 +3,25 @@ import { images } from "@/assets/Assets";
 const Partner = () => {
   return (
     <>
-      <div class="2xl:container 2xl:mx-auto md:py-12 lg:px-20 md:px-6 px-4 py-16 lg:py-20">
-        <h1 class="text-center lg:mb-20 mb-10 dark:text-white text-gray-800 font-semibold lg:text-4xl text-3xl lg:leading-9 leading-7 w-9/12 md:w-full mx-auto">
-          Our Channel Partner
-        </h1>
-        <div class="flex justify-around items-center m-auto  transition ease-out duration-700 ">
-          <img className="w-[40%]" src={images.Waree_Logo} alt="" />
+      <div className=" py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl  lg:py-20">
+        <div className="sm:text-center">
+          <h1 className=" mb-14 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            Our Channel Partner
+          </h1>
+          <div className="w-full flex flex-col md:flex-row gap-y-5 justify-around md:items-center items-start md:m-auto m-0  transition ease-out duration-700 ">
+            <img
+              className="w-[60%] md:w-[35%]"
+              src={images.Waree_Logo}
+              alt=""
+            />
 
-          <p className="w-[40%] text-gray-600 dark:text-gray-400 font-normal text-base leading-7">
-           " We proudly partner with Waaree, a leader in solar technology, to
-            deliver top-tier solar solutions. Together, we drive innovation and
-            sustainability, providing high-performance systems that exceed
-            expectations."
-          </p>
+            <p className="w-[80%] md:w-[40%] text-gray-600 dark:text-gray-400 font-normal text-base md:text-xl leading-7">
+              " We proudly partner with Waaree, a leader in solar technology, to
+              deliver top-tier solar solutions. Together, we drive innovation
+              and sustainability, providing high-performance systems that exceed
+              expectations."
+            </p>
+          </div>
         </div>
       </div>
     </>
