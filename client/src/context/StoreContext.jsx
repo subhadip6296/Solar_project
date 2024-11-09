3852f1756e1d4a8d8544a379e3211f5a657f0ec5
CK5115 <ckesharwani4@gmail.com>
2024-11-09 15:08:28 +0530

    commit

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -3,8 +3,8 @@ import { createContext, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:4000";
-  // const url = "https://gwinfra-server.onrender.com";
+  // const url = "http://localhost:4000";
+  const url = "https://gwinfra-server.onrender.com";
   const [token, setToken] = useState("");
   const contextValue = {
     url,
