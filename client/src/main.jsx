2014c84944cda6748db466f182ff56082dbdd6ad
CK5115 <ckesharwani4@gmail.com>
2024-11-15 12:20:29 +0530

    ready for review

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import StoreContextProvider from "./context/StoreContext";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StoreContextProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </StoreContextProvider>
+    <HelmetProvider>
+      <StoreContextProvider>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </StoreContextProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
