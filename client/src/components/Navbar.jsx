27c45b4968d05219a0624b84da2d9046667b8fe7
ashiii1 <aashishaik526@gmail.com>
2025-02-26 01:31:10 +0530

    dr

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -103,7 +103,7 @@ const Navbar = () => {
 
             {/* Theme Toggle & Mobile Menu Button */}
             <div className="flex items-center gap-4">
-              <motion.button
+              {/* <motion.button
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
                 onClick={() => setIsDark(!isDark)}
@@ -113,7 +113,7 @@ const Navbar = () => {
                 ) : (
                   <Moon className="w-5 h-5 text-gray-600" />
                 )}
-              </motion.button>
+              </motion.button> */}
 
               <motion.button
                 whileHover={{ scale: 1.1 }}
