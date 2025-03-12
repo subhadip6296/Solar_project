4fc015071ecd528c5d4c974f613dbb34a0ab5028
ashiii1 <aashishaik526@gmail.com>
2025-03-12 11:54:50 +0530

    gallery

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -282,9 +282,16 @@ const Navbar = () => {
 
               <div className="relative group">
                 <button className="flex items-center gap-2 text-gray-600 hover:text-[#118B50]">
+
+ 
+
+<Link to="/gallery" className="flex items-center gap-2 text-gray-600 hover:text-[#118B50]">
+
                   <BookImage className="w-5 h-5" />
-                  Gallery
+                   Gallery
                   <ChevronDown className="w-4 h-4" />
+                               </Link>
+
                 </button>
                 <div className="absolute left-0 mt-2 w-48 bg-white shadow-md rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
                   <button onClick={() => handleDownload("ServiceNowCSA.pdf")} className="block w-full text-left px-4 py-2 hover:bg-gray-100">
